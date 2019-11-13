fa5266a2980e7ae687783e6f3d821ec05687cb3f
Kavish Boyal <38506513+universekavish@users.noreply.github.com>
2019-11-13 18:07:52 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+   
+  <title>Kenya Prisons Service </title>
+  <link rel="stylesheet" media="screen" href="login.css" >
+</head>
+<body>
+
+	<table align="center" border="0" bgcolor="green" width="840" cellpadding="8" cellspacing="0" height="521">
+          <tr>
+            <td colspan="0" height="2"><img src="banner.gif""></td>
+          </tr>
+          <tr>
+            <td colspan="7" bgcolor="#yellow" height="1" align="center">
+		      <h1><font size="5">
+	           <a href="index.php">Home</a> |  	 
+		        <a href="login2.php">Member Login</a> |
+		        <a href="section.php">Prison Section</a> |
+		        <a href="location.php">About Us</a>|
+		        <a href="announce.php">Complain</a>
+		        </font></h1>
+            </td>
+</tr>
+<td width="91%" height="387" valign="top" bgcolor="#FFFFFF"><h2> Karen Campus</h2>
+  <p>Jomo Kenyatta University of Agriculture and Technology (JKUAT) KAREN CAMPUS was established in January 2006. It is located a few kilometers from Nairobi city Centre at the former Makini School premises next to Telkom Exchange (on Pala Close). It is set in a serene and beautiful neighborhood with adequate lecture rooms, Laboratory facilities and a capacity for over 4000 students.</p>
+  <p>The campus is about 30 Minutes drive from the City Center by private means or public service vehicles boarded at Ambassador plying route # 24 Karen-Hardy via Langata Road. One alights at Bogani Road Junction</p>
+  <p>.Currently the Campus does not have any accommodation facility, however there are private hostels strategically situated near the Campus where students make individual accommodation arrangements (More information on the private hostels is readily available from the office of the Student Counselor). </p>
+  <p><strong>jkuat buiding include library and all buiding</strong></p>
+    <div><img src="_44605982_prison226.jpg" width="226" height="170"  alt=""/><img src="jkuat bulidung.jpg" width="400" height="177"  alt=""/><img src="DSC_0024-300x199.jpg" width="304" height="178"  alt=""/></div></td>
+
+<td width="30%" height="387" valign="top" bgcolor="#FFFFFF">
+</td>
+<td bgcolor="#FFFFFF"></td>
+<td width="30%" bgcolor="#FFFFFF">
+
+<tr>
+<td bgcolor="#06640a" colspan="3" align="center">
+&copy; 2014 Kenya Prisons Service</td>
+</tr>
+</table>
+</body>
+</html>
